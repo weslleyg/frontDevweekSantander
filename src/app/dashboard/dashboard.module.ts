230cdafc8cbf3328c3bf7c906bb6d36e4dce9443
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-05-31 14:16:34 -0300

    integrating with the api

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
@@ -11,7 +12,8 @@ import { StockCardComponent } from './stock-card/stock-card.component';
     StockCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [
     DashboardPageComponent,
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,17 @@
+class Stock {
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.price = 0;
+    this.variation = 0;
+    this.date = '';
+  }
+
+  id: number;
+  name: string;
+  price: number;
+  variation: number;
+  date: string;
+}
+
+export default Stock;
